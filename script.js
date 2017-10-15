3d547d90ac640900128b0bab1981e4e1ba53a64e
ADIL KARMOUZI <adilkarmo@gmail.com>
2017-10-15 22:21:53 +0000

    add support for IE & MOZ | refactoring HTML & CSS files

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ $('.main-carousel').flickity({
   cellAlign: 'left',
   contain: true
 });
+
+var browser = navigator.userAgent.toLowerCase().indexOf('chrome') > -1 ? 'chrome' : 'other';
+if (BrowserDetect.browser.indexOf("explorer")>-1) {
+    document.write('<'+'link rel="stylesheet" href="../css/ie.css" />');
+}
